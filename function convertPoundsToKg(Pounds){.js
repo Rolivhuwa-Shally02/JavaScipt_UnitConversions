4d307dbf284e43db47c886d8288428c1fdd92a6a
Rolivhuwa-Shally02 <80606439+Rolivhuwa-Shally02@users.noreply.github.com>
2023-04-12 12:40:54 +0200

    Js files

---
+++ function convertPoundsToKg(Pounds){.js
@@ -0,0 +1,19 @@
+function convertPoundsToKg(Pounds){
+
+    return Pounds * 0.4535;
+    
+    };
+    
+    console.log("pounds to kilogram = " + convertPoundsToKg(500));
+    
+ function convertInchesToCm(Inches) {
+    return Inches * 2.54;
+
+ };
+ console.log("Inches to centimeters = " +convertInchesToCm(50));
+    
+  function fahrenheitToCelsius(Fahrenheit) {
+    return  (( Fahrenheit - 32) * 5/9) ;
+  };
+
+  console.log("Fahrenheit to Celsius = " + fahrenheitToCelsius(75) );
---
+++ index.js
@@ -0,0 +1,25 @@
+function convertPoundsToKg(Pounds){
+
+    return Pounds * 0.4535;
+    
+    };
+    console.log("pounds to kilogram = " + convertPoundsToKg(500));
+    
+ function convertInchesToCm(Inches) {
+    return Inches * 2.54;
+
+ };
+ console.log("Inches to centimeters = " +convertInchesToCm(50));
+    
+ function fahrenheitToCelsius(Fahrenheit) {
+    return  (( Fahrenheit - 32) * 5/9) ;
+  };
+
+  console.log("Fahrenheit to Celsius = " + fahrenheitToCelsius(75) );
+
+  function feetToMeters(Feet){
+  return Feet * 0.3048;
+
+  };
+  console.log("Feet to Meters = " + feetToMeters(10));
+
